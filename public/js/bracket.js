8b286fe29285e385a37ef748ea235e1e2736bb95
Chris <chrisg1821@gmail.com>
2021-05-29 09:27:11 -0700

    first round done javascript wise, need to finish east semi finals and finals while in transit

--- public/js/bracket.js
+++ public/js/bracket.js
@@ -1,107 +1,41 @@
-var $tournament = $('.tournament'),
-    $bracket = $('<ul class="bracket"><li></li><li></li></ul>');
+westRoundTwoChange = () => {
+    const west1 = document.getElementById("west1").value;
+    const west2 = document.getElementById("west2").value;
+    if ((west1 === "W1") && (west2 === "W2")) {
+        document.getElementById("w1VsW2").removeAttribute("class", "hide");
+    } else if ((west1 === "W1") && (west2 === "W3")) {
+        document.getElementById("w1VsW3").removeAttribute("class", "hide");
+    } else if ((west1 === "W4") && (west2 === "W2")) {
+        document.getElementById("w4VsW2").removeAttribute("class", "hide");
+    } else if ((west1 === "W4") && (west2 === "W3")) {
+        document.getElementById("w4VsW3").removeAttribute("class", "hide");
+    };
+};
 
-var participants = ['Adam', 'Matt', 'Evan', 'Abby', 'Heather', 'Christina', 'Ryan', 'Tyler', 'Steve', 'Steph', 'Jenna', 'Derek', 'Mike', 'Sam'];
+centralRoundTwoChange = () => {
+    const central1 = document.getElementById("central1").value;
+    const central2 = document.getElementById("central2").value;
+    if ((central1 === "C1") && (central2 === "C2")) {
+        document.getElementById("c1VsC2").removeAttribute("class", "hide");
+    } else if ((central1 === "C1") && (central2 === "C3")) {
+        document.getElementById("c1VsC3").removeAttribute("class", "hide");
+    } else if ((central1 === "C4") && (central2 === "C2")) {
+        document.getElementById("c4VsC2").removeAttribute("class", "hide");
+    } else if ((central1 === "C4") && (central2 === "C3")) {
+        document.getElementById("c4VsC3").removeAttribute("class", "hide");
+    };
+};
 
-function buildBracket($el, p1, p2) {
-    if (!p1 && !p2) {
-        $el.append($bracket.clone());
-    }
-}
-
-function cleanUp() {
-    var $brackets = $('.bracket'),
-        removed = false;
-    for (var i = 0; i < $brackets.length; i++) {
-        // break before there aren't enough spots
-        if ($('li:empty').length === participants.length) break;
-
-        var empty = true;
-        $brackets.eq(i).find('li').each(function () {
-            if (!$(this).is(':empty')) {
-                empty = false;
-            }
-        });
-        if (empty) {
-            $brackets.eq(i).remove();
-            removed = true;
-        }
-    }
-    return removed;
-}
-
-buildBracket($tournament);
-
-var level = 0,
-    $brackets,
-    $previousBrackets;
-
-// build 4 levels of brackets
-while (level < 4) {
-    $brackets = $('.bracket');
-
-    $brackets = $brackets.filter(function (i, el) {
-        if ($previousBrackets) {
-            if ($.inArray(el, $previousBrackets) >= 0) {
-                return false;
-            }
-        }
-        return true;
-    });
-
-    if (!$previousBrackets) {
-        $previousBrackets = $brackets;
-    }
-    else {
-        $previousBrackets = $.merge($previousBrackets, $brackets);
-    }
-
-    $brackets.each(function () {
-        $(this).find('li:empty').each(function () {
-            buildBracket($(this));
-        });
-    });
-
-    level++;
-}
-
-// remove empty lis until there are as many spots as participants
-while (cleanUp()) { }
-
-// add participants to empty lis
-var $empty = $('li:empty');
-for (var i = 0; i < participants.length; i++) {
-    $empty.eq(i).html('<button>' + participants[i] + '</button>');
-}
-
-// check if bracket has 2 winners, if so, change to buttons
-function changeToButtons() {
-    $('.bracket').each(function () {
-        var $winner = $(this).children('.winner');
-        if ($winner.length === 0) {
-            var $winners = $(this).children('li').children('ul').children('.winner');
-            if ($winners.length === 2) {
-                $winners.each(function () {
-                    $(this).html('<button class="winner">' + $(this).text() + '</button>');
-                });
-            }
-        }
-    });
-}
-
-$(document).on('click', 'button', function () {
-    var $ul = $(this).parent().parent();
-
-    if ($(this).hasClass('winner')) {
-        $ul = $ul.parent().parent();
-    }
-
-    $ul.append($('<li class="winner">' + $(this).text() + '</li>'));
-
-    // replace all buttons on clicked tier with text
-    $ul.find('button').each(function () {
-        $(this).replaceWith($(this).text());
-    });
-
-    changeToButtons();
-});
+eastRoundTwoChange = () => {
+    const east1 = document.getElementById("east1").value;
+    const east2 = document.getElementById("east2").value;
+    if ((east1 === "E1") && (east2 === "E2")) {
+        document.getElementById("e1VsE2").removeAttribute("class", "hide");
+    } else if ((east1 === "E1") && (east2 === "E3")) {
+        document.getElementById("e1VsE3").removeAttribute("class", "hide");
+    } else if ((east1 === "E4") && (east2 === "E2")) {
+        document.getElementById("e4VsE2").removeAttribute("class", "hide");
+    } else if ((east1 === "E4") && (east2 === "E3")) {
+        document.getElementById("e4VsE3").removeAttribute("class", "hide");
+    };
+};
